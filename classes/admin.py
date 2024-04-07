1108bae1261bc03506e3baa7dcae924a1f609331
Aimee Allen <aimeeallen94@gmail.com>
2024-04-07 16:40:48 +0000

    customised admin

--- classes/admin.py
+++ classes/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Level, Class_Type
 
 # Register your models here.
-admin.site.register(Level)
-admin.site.register(Class_Type)
+
+class LevelAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+class Class_TypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'level',
+        'teacher',
+        'day',
+        'time',
+        'rating',
+        'cost',
+    )
+
+admin.site.register(Level, LevelAdmin)
+admin.site.register(Class_Type, Class_TypeAdmin)
