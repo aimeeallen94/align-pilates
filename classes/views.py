9ca36d73c4f29ed205150ac921096d0d0ca533b4
Aimee Allen <aimeeallen94@gmail.com>
2024-04-07 16:52:09 +0000

    added class views and templates

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Class_Type
 
 # Create your views here.
+
+def all_classes(request):
+    """ A view to show the class/timetable page and all for some filtering """
+
+    class_types = Class_Type.objects.all
+
+    context = {
+        'class_types': class_types,
+    }
+
+    return render(request, 'classes/classes.html', context )
