d4c32a1fdc549e8b51f120713beb9ce859593168
Aimee Allen <aimeeallen94@gmail.com>
2024-04-09 10:48:13 +0000

    add class detail view, urls and template and styling for same

--- classes/urls.py
+++ classes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_classes, name='classes'),
+    path('<int:class_type_id>', views.class_info, name='class_type'),
 ]
--- classes/views.py
+++ classes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Class_Type
 
 # Create your views here.
@@ -13,3 +13,15 @@ def all_classes(request):
     }
 
     return render(request, 'classes/classes.html', context )
+
+
+def class_info(request, class_type_id):
+    """ A view to show the class descriptions page """
+
+    class_type = get_object_or_404(Class_Type, pk=class_type_id)
+
+    context = {
+        'class_type': class_type,
+    }
+
+    return render(request, 'classes/class_type.html', context )
