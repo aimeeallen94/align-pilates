fe1a82350c9de6f1a89e739a9e33de17fda8bbaa
Aimee Allen <aimeeallen94@gmail.com>
2024-04-10 15:13:07 +0000

    adding stripe to checkout page

--- basket/contexts.py
+++ basket/contexts.py
@@ -11,15 +11,16 @@ def basket_contents(request):
     cost = 15
     basket = request.session.get('basket', {})
 
-    for class_id, class_count in basket.items():
-        booking = get_object_or_404(Class_Type, pk=class_id)
-        grand_total = class_count * 15
-        class_count += class_count
-        basket_items.append({
-            'class_id': class_id,
-            'class_count': class_count,
-            'booking': booking,
-        })
+    for class_id, item_data in basket.items():
+        if isinstance(class_count, int):
+            booking = get_object_or_404(Class_Type, pk=class_id)
+            grand_total = class_count * 15
+            class_count += item_data
+            basket_items.append({
+                'class_id': class_id,
+                'class_count': item_data,
+                'booking': booking,
+            })
 
     context = {
         'basket_items': basket_items,
