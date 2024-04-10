7487db24a76804f7f08cad12672d95159fd0cf38
Aimee Allen <aimeeallen94@gmail.com>
2024-04-10 13:08:49 +0000

    created checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import ReservationForm
+
+def checkout(request):
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, 'Your shopping bag is empty')
+        return redirect(reverse('classes'))
+
+    reservation_form = ReservationForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'reservation_form': reservation_form,
+    }
+
+    return render(request, template, context)
