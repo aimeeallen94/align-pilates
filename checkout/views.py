9c08a21bc436d178e7e6f2aa373175cb85e24ae4
Aimee Allen <aimeeallen94@gmail.com>
2024-04-10 16:22:58 +0000

    set up stripe funcionality and form submission

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import ReservationForm
+from basket.contexts import basket_contents
+
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, 'Your shopping bag is empty')
         return redirect(reverse('classes'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+
     reservation_form = ReservationForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'reservation_form': reservation_form,
-        'stripe_public_key': 'pk_test_51P41YxKxh0iNjxuCiVPsxJ4jfhxyjqfokMyglR9KQ8ZMu01EqrQAkqNUlMPk4PZ1Ef0kTsRqvK06HwfnLZPFIJWu00hWm7ZmMr',
-        'client_secret': 'client secret key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
