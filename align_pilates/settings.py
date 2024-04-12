9323f9cd03a846d5f0cd2542ff6b9b5b47672993
Aimee Allen <aimeeallen94@gmail.com>
2024-04-12 18:55:53 +0000

    removed secret key and changed debug value

--- align_pilates/settings.py
+++ align_pilates/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)s(8+axt9(*z%vhc7%ou7^hn@688q=$zjaxb^&n6hi$$rcf^3)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-aimeeallen9-alignpilate-iwcfd67hn0r.ws-eu110.gitpod.io', 'align-pilates.heroku.app.com', 
                 'localhost', 'align-pilates-3ba4de4e758e.herokuapp.com',]
